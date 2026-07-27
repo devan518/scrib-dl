84725433fd0240cf0fa29d77058f0ff7ab0f6e11
devan518 <yourgithubemail@example.com>
2026-07-27 19:17:08 -0400

    enhanced project structure to be downloaded with pipx

---
+++ INSTALL.md
@@ -0,0 +1,11 @@
+# HOW TO INSTALL
+
+run these commands:
+
+```
+pipx install --include-deps "git+https://github.com/devan518/scrib-dl.git"
+playwright install chromium
+scrib-dl
+```
+
+Ensure you have pipx installed!
--- scribd.py
+++ scribd.py
@@ -2,53 +2,57 @@ from playwright.sync_api import sync_playwright
 from pathlib import Path
 import img2pdf
 
-output_dir = Path("screenshots")
-output_dir.mkdir(exist_ok=True)
-link = input("SCRIB-DL\ninput scribd link: ")
-with sync_playwright() as p:
-    browser = p.chromium.launch()
+def main():
+    output_dir = Path("screenshots")
+    output_dir.mkdir(exist_ok=True)
+    link = input("SCRIB-DL\ninput scribd link: ")
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
 
-    page = browser.new_page(viewport={"width": 1600, "height": 3000})
-    print("going to scribd file..")
+        page = browser.new_page(viewport={"width": 1600, "height": 3000})
+        print("going to scribd file..")
 
-    page.goto(link, wait_until="domcontentloaded")
-    print("loaded DOM")
+        page.goto(link, wait_until="domcontentloaded")
+        print("loaded DOM")
 
-    uploader = page.get_by_label("Uploaded by").locator("..").locator("a").nth(0).inner_text()
-    print(f"Uploaded by {uploader}")
+        uploader = page.get_by_label("Uploaded by").locator("..").locator("a").nth(0).inner_text()
+        print(f"Uploaded by {uploader}")
 
-    title = page.title()
-    print(f"File: {title}")
+        title = page.title()
+        print(f"File: {title}")
 
-    description = page.locator('[data-e2e="description"] > div').nth(0).inner_text()
-    print(f"Description:\n{description}")
+        description = page.locator('[data-e2e="description"] > div').nth(0).inner_text()
+        print(f"Description:\n{description}")
 
-    #scroll to bypass scribd's lazy loading system that requires you to scroll the ENTIRE THING for all of the pages to load
-    for i in range(30):
-        page.mouse.wheel(0, 1000)
-        page.wait_for_timeout(500)
-    print("preloaded pages, downloading images of each page")
-    pages = page.locator(".newpage")
-    count = pages.count()
-    print(f"found {count} pages")
+        #scroll to bypass scribd's lazy loading system that requires you to scroll the ENTIRE THING for all of the pages to load
+        for i in range(30):
+            page.mouse.wheel(0, 1000)
+            page.wait_for_timeout(500)
+        print("preloaded pages, downloading images of each page")
+        pages = page.locator(".newpage")
+        count = pages.count()
+        print(f"found {count} pages")
 
-    for i in range(count):
-        div = pages.nth(i)
+        for i in range(count):
+            div = pages.nth(i)
 
-        div.scroll_into_view_if_needed()
-        page.wait_for_timeout(500)
+            div.scroll_into_view_if_needed()
+            page.wait_for_timeout(500)
 
-        div.screenshot(path=str(output_dir / f"page_{i + 1}.png"))
-        print(f"Saved page_{i + 1}.png")
-    screenshots = list(Path("screenshots").iterdir())
-    with open(f"output.pdf", "wb") as f:
-        f.write(
-            img2pdf.convert(
-                screenshots,
-                author=uploader,
-                title=title,
-                subject=description,
+            div.screenshot(path=str(output_dir / f"page_{i + 1}.png"))
+            print(f"Saved page_{i + 1}.png")
+        screenshots = list(Path("screenshots").iterdir())
+        with open(f"output.pdf", "wb") as f:
+            f.write(
+                img2pdf.convert(
+                    screenshots,
+                    author=uploader,
+                    title=title,
+                    subject=description,
+                )
             )
-        )
-    print("saved output.pdf")
-    browser.close()
+        print("saved output.pdf")
+        browser.close()
+
+if __name__ == "__main__":
+    main()
