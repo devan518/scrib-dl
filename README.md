2bfbccd18b307313909d752a18f47928fdefd875
devan518 <yourgithubemail@example.com>
2026-07-27 17:50:16 -0400

    added pdf metadata!

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is a project for the students who might need it (including me)
 
+> **Note:** the pages may or may not be in order, and may include the ad overlays, will patch
+
 ## what does it do?
 
 You paste in the scribd url and it outputs a pdf in the `/screenshots` directory!
@@ -11,8 +13,8 @@ You paste in the scribd url and it outputs a pdf in the `/screenshots` directory
 It uses [playwright](https://playwright.dev/python/) and screenshots each page, then it uses [img2pdf](https://pypi.org/project/img2pdf/) to create a pdf
 with those images
 
-## why is it only one commit?
+### acknowledgements!
 
-i worked on it and then i uploaded it to github after i finished
-because i forgot to, even if i did use github in the start
-it would still be one commit because its a really simple file
+- [example pdf used](https://www.scribd.com/document/870826699/Solution-to-CSEC-Math-Jan-2025-P1)
+- [playwright](https://playwright.dev/python/)
+- [img2pdf](https://pypi.org/project/img2pdf/)
--- scribd.py
+++ scribd.py
@@ -7,10 +7,22 @@ output_dir.mkdir(exist_ok=True)
 link = input("SCRIB-DL\ninput scribd link: ")
 with sync_playwright() as p:
     browser = p.chromium.launch()
+
     page = browser.new_page(viewport={"width": 1600, "height": 3000})
     print("going to scribd file..")
+
     page.goto(link, wait_until="domcontentloaded")
     print("loaded DOM")
+
+    uploader = page.get_by_label("Uploaded by").locator("..").locator("a").nth(0).inner_text()
+    print(f"Uploaded by {uploader}")
+
+    title = page.title()
+    print(f"File: {title}")
+
+    description = page.locator('[data-e2e="description"] > div').nth(0).inner_text()
+    print(f"Description:\n{description}")
+
     #scroll to bypass scribd's lazy loading system that requires you to scroll the ENTIRE THING for all of the pages to load
     for i in range(30):
         page.mouse.wheel(0, 1000)
@@ -30,6 +42,13 @@ with sync_playwright() as p:
         print(f"Saved page_{i + 1}.png")
     screenshots = list(Path("screenshots").iterdir())
     with open(f"output.pdf", "wb") as f:
-        f.write(img2pdf.convert(screenshots))
+        f.write(
+            img2pdf.convert(
+                screenshots,
+                author=uploader,
+                title=title,
+                subject=description,
+            )
+        )
     print("saved output.pdf")
     browser.close()
